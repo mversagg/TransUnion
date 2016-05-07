2d791e468d69c80580e0eb84702d54b7308aa45f
connor <connorkrier@gmail.com>
2016-05-07 13:36:19 -0700

    started adding logic

--- js/table.js
+++ js/table.js
@@ -3,6 +3,7 @@ let colors = [ 'purple', 'plum', 'monza',
   	'pict_blue', 'royal_blue', 'cool_green', 'gossip', 'salem', 'fire_bush',
   	'light_orange', 'mid_orange', 'dark_pink', 'turq', 'aqua', 'butter'];
 let icons = ['fa-blind', 'fa-binoculars', 'fa-bomb', 'fa-code', 'fa-eye', 'fa-send', 'fa-photo', 'fa-television', 'fa-tags'];
+let rankIndex = 1;
 
 $(function() {
 	$('td').click(gameController);
@@ -25,45 +26,24 @@ function randomize() {
 			icon.addClass('fa ' + icons[index]);
 			icon.addClass('fa-2x');
 		});
-
-		/*if (index > 0) {
-			setTimeout(function() {
-				tile.fadeOut(200, function() {
-					let temp = $(this);
-					temp.removeClass();
-					temp.addClass('tile ' + colors[index]);
-
-					$('.fa').each(function(index) {
-						let icon = $(this);
-						icon.removeClass();
-						icon.addClass('fa ' + icons[index]);
-					});
-
-					tile.fadeIn(200);
-				});
-			}, delay);
-
-			delay += INCR;
-		} else {
-			tile.fadeOut(200, function() {
-				let temp = $(this);
-				temp.removeClass();
-				temp.addClass('tile ' + colors[index]);
-
-				$('.fa').each(function(index) {
-					let icon = $(this);
-					icon.removeClass();
-					icon.addClass('fa ' + icons[index]);
-				});
-				tile.fadeIn(200);
-			});
-		}*/
 	});
 }
 
 function gameController(event) {
-	randomize();
+	console.log(event);
+	let row = event.currentTarget.dataset.row;
+	let col = event.currentTarget.dataset.col;
+	let clickIcon = $(event.currentTarget.childNodes[0])[0].classList[1];
 
+	$('.tracker').each(function(index) {
+		let rank = $(this);
+
+		if (rank.data('rank') == rankIndex) {
+			rank.addClass(clickIcon + ' filled');
+		}
+	});
+
+	randomize();
 }
 
 function shuffle(a) {
